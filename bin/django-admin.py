5c9a3e846c42d2d58d5d36f07e5c5934fb22edef
despicableGruu <brandon.richardson4328@outlook.com>
2017-01-03 19:14:26 -0800

    Actually removing the files

--- bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/home/bmuse/Django-Projects/PDiPortal2/bin/python3
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
--- lib/python3.4/site-packages/review/tests/test_app/models.py
+++
@@ -1,5 +0,0 @@
-from django.db import models
-
-
-class WeatherCondition(models.Model):
-    name = models.CharField(max_length=20)
