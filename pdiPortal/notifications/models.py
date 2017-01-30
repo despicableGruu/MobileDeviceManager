9547faebb6816a637ff62d014f7104f280e7b95e
despicableGruu <brandon.richardson4328@outlook.com>
2017-01-30 10:19:34 -0800

    Added a notifications application.
    This application will handle sending a variety of notifications to users of the website.

---
+++ pdiPortal/notifications/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+from profiles.models import PortalUser
+
+# Create your models here.
+class notification(models.Model):
+  """Notifications for users"""
+  title = models.CharField(max_length=25)
+  body = models.TextField(max_length=500)
+  action_url = models.URLField()
+  user = models.ForeignKey(PortalUser)
+
+  def __str__():
+    return self.title
