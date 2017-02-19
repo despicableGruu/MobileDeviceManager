3de58abaeda2b8d10f05bf428c5bfc02de34e80a
despicableGruu <brandon.richardson4328@outlook.com>
2017-02-18 23:19:47 -0800

    Revert "Removing Device model table."
    This reverts commit ac22d8dea27ade18e7d54fea8d8834340b1ca6bf.

--- pdiPortal/device/models.py
+++ pdiPortal/device/models.py
@@ -4,6 +4,14 @@ from profiles.models import PortalUser
 
 DEFAULT_FACILITY_ID = 1
 
+# Create your models here.
+class DeviceModel(models.Model):
+    """A class that holds all of the device models"""
+    device_model = models.CharField(max_length=40)
+
+    def __str__(self):
+        return self.device_model
+
 class Device(models.Model):
     """A class that stores all of the data about a device in the database."""
     name = models.CharField(max_length=100, default='Android Device')
@@ -12,7 +20,7 @@ class Device(models.Model):
     operating_system_version = models.CharField(max_length=25)
     user = models.ForeignKey(PortalUser)
     facility = models.ForeignKey(Facility, default=DEFAULT_FACILITY_ID)
-    device_model = models.CharField(max_length=40)
+    device_model = models.ForeignKey(DeviceModel)
     heartbeat = models.BooleanField(default=False)
 
     def __str__(self):
