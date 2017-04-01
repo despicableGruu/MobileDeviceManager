211806d3556a2579af3e5d39eb6817f0d413d3ea
despicableGruu <brandon.richardson4328@outlook.com>
2017-04-01 00:13:17 -0700

    Adding a TimeStampedModel
    This commit adds a time stamped abstract model that can be included in models to make auto generated and updated 'created' and 'modified' fields.

---
+++ pdiPortal/core/models.py
@@ -0,0 +1,15 @@
+"""
+Core models to be included in multiple applications
+"""
+
+from django.db import models
+
+class TimeStampedModel(models.Model):
+    """
+    An abstract base class model that provides self-updating created and modified fields.
+    """
+    created = models.DateTimeField(auto_now_add=True)
+    modified = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
